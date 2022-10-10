c1f97535074a8e7bf4877e5b13d09517730f8e92
Bom <11377ny@gmail.com>
2022-10-09 20:05:07 -0400

    Update Q1672

--- 1601-1700/1672. Richest Customer Wealth.py
+++ 1601-1700/1672. Richest Customer Wealth.py
@@ -4,3 +4,14 @@ class Solution:
         for account in accounts:
             rich = max(sum(account), rich)
         return rich
+
+# class Solution:
+#     def maximumWealth(self, accounts: List[List[int]]) -> int:
+#         wealthes = []
+#         total = 0
+#         for i in range(len(accounts)):
+#             for x in range(len(accounts[i])):
+#                 total += accounts[i][x]
+#             wealthes.append(total)
+#             total = 0
+#         return max(wealthes)
